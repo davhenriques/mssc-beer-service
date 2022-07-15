0f31e14c4eef500a82971fc1406782e31d8db22e
BidinhoSousa <davidhenriquesdesousa@gmail.com>
2022-07-15 19:17:48 +0100

    Added Beer JPA Entity. Closes #5

--- src/test/java/davhenriques/spring/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/davhenriques/spring/msscbeerservice/web/controller/BeerControllerTest.java
@@ -7,11 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
